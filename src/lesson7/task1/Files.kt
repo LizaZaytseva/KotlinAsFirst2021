d2f5354e1b8c8ee2cfa41292468cc776910ddaf9
dungeonMaster <10raven11@gmail.com>
2021-11-30 21:29:34 +0300

    html fixes

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -434,7 +434,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     }
 
     for (line in File(inputName).readLines()) {
-        if (line.isEmpty() || (line.matches(Regex("[\t ]")))) {
+        if (line.isEmpty()) {
             if (!prevWasEmpty) {
                 writer.newLine()
                 writer.write("</p>")
